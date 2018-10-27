4555d5620af78e935ef9791ab6719a856a0d137f
Viet Hung Nguyen <hvn@familug.org>
2018-10-27 15:19:48 +0700

    Add super simple web app using plug cowboy

---
+++ my_app/lib/my_app/application.ex
@@ -0,0 +1,21 @@
+defmodule MyApp.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: MyApp.Worker.start_link(arg)
+      # {MyApp.Worker, arg},
+      Plug.Cowboy.child_spec(scheme: :http, plug: MyRouter, options: [port: 4001])
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: MyApp.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
